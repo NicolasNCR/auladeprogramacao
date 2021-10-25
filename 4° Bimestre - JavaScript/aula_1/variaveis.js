2be826feb63b2416bcbb03e6f376c3c6cb5632f6
Nicolas Costa Ribeiro <79952586+NicolasNCR@users.noreply.github.com>
2021-10-25 16:57:19 -0300

    4 Bimestre - JavaScript

---
+++ 4° Bimestre - JavaScript/aula_1/variaveis.js
@@ -0,0 +1,5 @@
+var num = 10;
+var num2 = 200;
+var texto = "Campanha Arrecadação" 
+var cliente = "Nicolas Costa Ribeiro"
+pontos = num + num2;
---
+++ 4° Bimestre - JavaScript/aula_2/entrada.js
@@ -0,0 +1,6 @@
+function pedirvalores(){
+    num1 = parseInt(prompt("Digite o primeiro número"))
+    num2 = parseInt(prompt("Digite o segundo número"))
+    soma = num1+num2
+    subtracao = num1-num2
+} 
